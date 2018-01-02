4cf6de9f5237b363e4eaf5a1c87d3b280be63329
Alex Hoppe <amhoppe@alexs-mbp.attlocal.net>
2018-01-02 18:21:51 -0500

    First run at Trivia Game

---
+++ assets/javascript/game.js
@@ -0,0 +1,27 @@
+$( document ).ready(function(){
+  // To make Trivia Game need to count correct and incorrect answers
+
+
+  // Need to compare question choices and answers
+
+
+  // Need to compare User Choices and correct answers
+
+
+  // Need to write to HTML
+
+
+  // Step 1 create HTML with all the questions listed.
+
+      // Questions will have buttons with id's called by javascript
+
+      // on.click will ad to a counter of correct or incorrect
+
+      // running score will be held at the bottom
+
+
+  // Step 2 print running score if all the questions have been answered
+
+      // need a script using if statement to count the questions answered
+
+      
